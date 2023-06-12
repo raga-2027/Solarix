fb8583ed76db839072bcb3e8a30521ba32203585
raga-2027 <132984150+raga-2027@users.noreply.github.com>
2023-06-12 16:50:41 -0600

    subo proyecto

---
+++ Home_Solarix_files/Home_solarix.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 80, 
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 8000, 
+    },
+  });
+  
+  
+  
