1bb04823e89dbb6e34e46276ce5dbe44d87c0038
raga-2027 <132984150+raga-2027@users.noreply.github.com>
2023-06-13 19:11:15 -0600

    actualisacion

--- Home_Solarix_files/Home_solarix.js
+++ Home_Solarix_files/Home_solarix.js
@@ -1,16 +1,26 @@
 var swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    slidesPerView: 3,
-    spaceBetween: 80, 
     loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
     autoplay: {
-      delay: 8000, 
+      delay: 8000, // Cambiar de imagen cada 8 segundos
     },
   });
+
   
+  document.getElementById("mostrarImagen").addEventListener("click", function() {
+  document.getElementById("imagenOculta").src = "ruta-de-la-imagen.jpg";
+});
+
+function showContent(index) {
+    var images = document.getElementsByClassName('image');
+    var lists = document.getElementsByClassName('list');
+    for (var i = 0; i < images.length; i++) {
+      images[i].classList.remove('active');
+      lists[i].classList.remove('active');
+    }
   
+    var image = document.getElementById('image' + index);
+    var list = document.getElementById('list' + index);
+    image.classList.add('active');
+    list.classList.add('active');
+  }
   
